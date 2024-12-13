c41e2a16840691f1436763cec481eeba777b971b
owhenthesaints <owen.simon@epfl.ch>
2024-12-13 11:17:01 +0100

    build(ploting_all_plots): modularized code

---
+++ _javascript/ploting/plot_country.js
@@ -0,0 +1,25 @@
+import csvData from '../../data/origin_stats.csv';
+export function plotMap() {
+
+
+  /** @type {CsvDataRow[]}*/
+  const typedCsvData = csvData;
+
+  let data = [{
+    type: 'choropleth',
+    locationmode: 'country names',
+    locations: typedCsvData.map(row => row['Origin']),
+    z: typedCsvData.map(row => row['Mean_grade']),
+    autocolorscale: true
+  }]
+
+// Layout
+  const layout = {
+    title: 'Simple Line Chart',
+    xaxis: {title: 'X Axis Label'},
+    yaxis: {title: 'Y Axis Label'}
+  };
+
+// Render Plot
+  Plotly.newPlot('specific', data, layout);
+}
--- _javascript/ploting_all_plots.js
+++ _javascript/ploting_all_plots.js
@@ -1,20 +1,12 @@
-import csvData from '../data/mean_grade_per_type_1.csv'
+/**
+ * @typedef {Object} CsvDataRow
+ * @property {string} Origin
+ * @property {number} Mean_grade
+*/
+
+import {plotMap} from './ploting/plot_country.js';
 
 
-let data = [{
-  type : 'choropleth',
-  locationmode: 'country names',
-  locations: csvData.map(row => row['Origin']),
-  z: csvData.map(row => row['Mean_grade']),
-  autocolorscale: true
-}]
 
-// Layout
-const layout = {
-  title: 'Simple Line Chart',
-  xaxis: { title: 'X Axis Label' },
-  yaxis: { title: 'Y Axis Label' }
-};
 
-// Render Plot
-Plotly.newPlot('specific', data, layout);
+plotMap();
