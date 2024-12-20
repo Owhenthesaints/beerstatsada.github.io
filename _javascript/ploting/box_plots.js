a38aedcb1239940ba28d37d6e6a95f5a5f4350c8
owhenthesaints <owen.simon@epfl.ch>
2024-12-20 14:15:03 +0100

    fix(box_plots): only available countries show up on beer_type select

--- _javascript/ploting/box_plots.js
+++ _javascript/ploting/box_plots.js
@@ -1,5 +1,16 @@
 import price_dataset from '../../data/data_for_boxplot.csv';
 
+function filter_data(beer_type, country) {
+  let data_for_plot = JSON.parse(JSON.stringify(price_dataset));
+  if (beer_type !== 'All') {
+    data_for_plot = data_for_plot.filter(row => row['Beer_type'] === beer_type);
+  }
+  if (country !== 'All') {
+    data_for_plot = data_for_plot.filter(row => row['Origin'] === country);
+  }
+  return data_for_plot;
+}
+
 function populateSelectorBeerType() {
   let selector = document.getElementById('box_plot_selector_type');
   let unique_beer_types = [...new Set(price_dataset.map(row => row['Beer_type']))];
@@ -14,12 +25,19 @@ function populateSelectorBeerType() {
     option.text = beer_type;
     selector.add(option);
   });
+
   return selector;
 }
 
-function populateSelectorCountry() {
+function populateSelectorCountry(beer_type, country) {
   let selector = document.getElementById('box_plot_selector_country');
-  let unique_countries = [...new Set(price_dataset.map(row => row['Origin']))];
+  while (selector.firstChild) {
+    selector.removeChild(selector.firstChild);
+  }
+  // get the unique countries out of filtered data
+  let data_for_selector = filter_data(beer_type, 'All');
+  let unique_countries = [...new Set(data_for_selector.map(row => row['Origin']))];
+  // add menu options
   let option = document.createElement('option');
   // add All
   option.text = 'All';
@@ -31,29 +49,30 @@ function populateSelectorCountry() {
     option.text = country;
     selector.add(option);
   });
+  let options = selector.options;
+  for (let i = 0; i < options.length; i++) {
+    if (options[i].value === country) {
+      options[i].selected = true;
+      break;
+    }
+  }
   return selector;
 }
 
+
 function boxPlotData(beer_type, country) {
-  // deep copy
-  let data_for_plot = JSON.parse(JSON.stringify(price_dataset));
-  if (beer_type !== 'All') {
-    data_for_plot = data_for_plot.filter(row => row['Beer_type'] === beer_type);
-  }
-  if (country !== 'All') {
-    data_for_plot = data_for_plot.filter(row => row['Origin'] === country);
-  }
+  let data_for_plot = filter_data(beer_type, country);
   let unique_countries = [...new Set(data_for_plot.map(row => row['Origin']))];
   // basically give back all the prices per country
   return unique_countries.map(country => [country, data_for_plot.filter(row => row['Origin'] === country).map(row => row['Price_per_litter'])]);
 }
 
 export function plotBoxPlot() {
-  let type_selector = populateSelectorBeerType();
-  let country_selector = populateSelectorCountry();
   let type= 'All';
   let country = 'All';
   refreshBoxPlot();
+  let type_selector = populateSelectorBeerType();
+  let country_selector = populateSelectorCountry(type, country);
 
   function refreshBoxPlot()
   {
@@ -75,6 +94,7 @@ export function plotBoxPlot() {
       }
     });
   }
+
   function dropmenuCallback(event) {
     if (event.target.id === 'box_plot_selector_type') {
       type = event.target.value;
@@ -82,6 +102,7 @@ export function plotBoxPlot() {
       country = event.target.value;
     }
     refreshBoxPlot();
+    populateSelectorCountry(type, country);
   }
   type_selector.addEventListener('change', dropmenuCallback);
   country_selector.addEventListener('change', dropmenuCallback);
---
+++ _javascript/ploting/time_series.js
@@ -0,0 +1,6 @@
+import time_series_rb from '../../data/seasonal_trends_sample_RB.csv';
+import time_series_ba from '../../data/seasonal_trends_sample_BA.csv';
+
+export function plotTimeSeries() {
+
+}
